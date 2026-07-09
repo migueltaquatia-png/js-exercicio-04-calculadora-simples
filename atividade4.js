089eb7455294abb74438cc25dea51b158ba27887
miguel rosa taquatia <miguelito23rosa@gmail.com>
2026-07-09 16:58:58 -0300

    atividade 4

---
+++ atividade4.js
@@ -0,0 +1,7 @@
+let numero1 = 10;
+let numero2 = 5;
+
+console.log("Soma: " + (numero1 + numero2));
+console.log("Subtração: " + (numero1 - numero2));
+console.log("Multiplicação: " + (numero1 * numero2));
+console.log("Divisão: " + (numero1 / numero2));
